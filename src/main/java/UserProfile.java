cb9b828f2c832f01ebd04e989179d873ae34c63b
Navid Ibrahim <nibrahim7787@gmail.com>
2025-03-30 12:43:23 +0600

    Added method to update profile

--- src/main/java/UserProfile.java
+++ src/main/java/UserProfile.java
@@ -1,23 +1,20 @@
-abstract class UserData {
+abstract class UserProfile {
     private int age;
     private double heightM;
     private double weightKG;
     private final String gender;
-    
+    private int activityLevel;
 
-    public UserData(int age, double heightM, double weightKG, String gender) {
+    public UserProfile(int age, double heightM, double weightKG, String gender, int activityLevel) {
         this.age = age;
         this.heightM = heightM;
         this.weightKG = weightKG;
         this.gender = gender;
+        this.activityLevel = activityLevel;
     }
 
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public double getHeightM() {
@@ -28,19 +25,26 @@ abstract class UserData {
         return heightM * 100;
     }
 
-    public void setHeightM(double heightM) {
-        this.heightM = heightM;
-    }
-
     public double getWeightKG() {
         return weightKG;
     }
 
-    public void setWeightKG(double weightKG) {
-        this.weightKG = weightKG;
-    }
-
     public String getGender() {
         return gender;
+    }
+
+    public int getActivityLevel() {
+        return activityLevel;
+    }
+
+    public void setActivityLevel(int activityLevel) {
+        this.activityLevel = activityLevel;
+    }
+
+    public void updateProfile(int age, double heightM, double weightKG, int activityLevel) {
+        this.age = age;
+        this.heightM = heightM;
+        this.weightKG = weightKG;
+        this.activityLevel = activityLevel;
     }
 }
