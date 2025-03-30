1455fa86b7a0d195af5c6ec094fe008662ed8b71
Navid Ibrahim <nibrahim7787@gmail.com>
2025-03-30 23:02:23 +0600

    Added method to clear console

--- src/main/java/UIHelper.java
+++ src/main/java/UIHelper.java
@@ -74,4 +74,22 @@ public class UIHelper {
         return register();
     }
 
+    public void clear() {
+        System.out.println("\n\nPress Enter to continue...");
+        InputHelper.readLine();
+        clearConsole();
+    }
+
+    private void clearConsole(){
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
